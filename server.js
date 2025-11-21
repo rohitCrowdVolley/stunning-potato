248687ac80bd91cfe8335415eb2e8157373a1ac0
rohit <rohit@crowdvolley.com>
2025-11-21 11:43:14 +0530

    d

--- server.js
+++ server.js
@@ -3,19 +3,19 @@ const xmlparser = require("express-xml-bodyparser");
 
 const app = express();
 
-app.use(express.json());
+// app.use(express.json());
 
 app.use(xmlparser());
 
-app.post("/access-token", async (req, res) => {
-    console.log("Received body:", req.body);
-    return res.send("OK");
-});
+// app.post("/access-token", async (req, res) => {
+//     console.log("Received body:", req.body);
+//     return res.send("OK");
+// });
 
-app.post("/user", async (req, res) => {
-    console.log("Received body:", req.body);
-    return res.send("OK");
-});
+// app.post("/user", async (req, res) => {
+//     console.log("Received body:", req.body);
+//     return res.send("OK");
+// });
 
 app.post("/companies", async (req, res) => {
     console.log("Received body:", req.body);
